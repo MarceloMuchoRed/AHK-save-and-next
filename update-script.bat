bb74130e26f66129ee4c1660ec71491ec1055563
MarceloMuchoRed <marcelod@muchored.com>
2026-06-19 08:56:14 -0600

    Delete update-script.bat

--- update-script.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-curl.exe -o "C:\Users\p-mdragustinovis\Documents\AutoHotkey\AHK save and next\AHK_save_and_next.ahk" https://raw.githubusercontent.com/MarceloMuchoRed/AHK-save-and-next/main/save-and-next.ahk
-echo Done! Script updated.
-pause
